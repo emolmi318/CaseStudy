d81c27b2f81593909171a7607e5d7461e9e9ccc8
emolmi318 <michaee5@wwu.edu>
2025-05-07 16:01:48 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -1,5 +1,88 @@
 ##########################
 
+# Download the required packages
+library(shiny)
+library(vroom)
+library(tidyverse)
+
+# Download the required data
+dir.create("neiss")
+#> Warning in dir.create("neiss"): 'neiss' already exists
+download <- function(name) {
+  url <- "https://raw.github.com/hadley/mastering-shiny/main/neiss/"
+  download.file(paste0(url, name), 
+                paste0("neiss/", name), 
+                quiet = TRUE)
+}
+download("injuries.tsv.gz")
+download("population.tsv")
+download("products.tsv")
+
+injuries <- vroom::vroom(
+  "neiss/injuries.tsv.gz")
+injuries
+
+
+products <- vroom::vroom("neiss/products.tsv")
+products
+
+population <- vroom::vroom(
+  "neiss/population.tsv")
+population
+
+selected <- injuries %>% filter(
+  prod_code == 649)
+nrow(selected)
+
+selected %>% count(
+  location, wt = weight, sort = TRUE)
+
+selected %>% count(
+  body_part, wt = weight, sort = TRUE)
+
+selected %>% count(
+  diag, wt = weight, sort = TRUE)
+
+summary <- selected %>% 
+  count(age, sex, wt = weight)
+summary
+
+summary %>% 
+  ggplot(aes(age, n, colour = sex)) + 
+  geom_line() + 
+  labs(y = "Estimated number of injuries")
+
+summary <- selected %>% 
+  count(age, sex, wt = weight) %>% 
+  left_join(
+    population, by = c("age", "sex")) %>% 
+  mutate(rate = n / population * 1e4)
+
+summary
+
+summary %>% 
+  ggplot(aes(age, rate, colour = sex)) + 
+  geom_line(na.rm = TRUE) + 
+  labs(y = "Injuries per 10,000 people")
+
+selected %>% 
+  sample_n(10) %>% 
+  pull(narrative)
+
+injuries %>%
+  mutate(diag = fct_lump(fct_infreq(diag), 
+                         n = 5)) %>%
+  group_by(diag) %>%
+  summarise(n = as.integer(sum(weight)))
+
+count_top <- function(df, var, n = 5) {
+  df %>%
+    mutate({{ var }} := fct_lump(
+      fct_infreq({{ var }}), n = n)) %>%
+    group_by({{ var }}) %>%
+    summarise(n = as.integer(sum(weight)))
+}
+
 # Required Packages
 library(shiny)
 library(shinythemes) # First time trying this!
@@ -24,6 +107,8 @@ ui <- fluidPage(
     column(2, selectInput("y", "Y axis", c(
       "rate", "count"))) # Option to choose rate/count
   ),
+  numericInput( # Question 3
+    "n_rows", "Number of rows to display:", value = 5, min = 1, max = 20),
   fluidRow( # Will show in table output
     column(4, tableOutput("diag")),
     column(4, tableOutput("body_part")),
@@ -33,10 +118,15 @@ ui <- fluidPage(
   fluidRow( # Interactive plot
     column(12, plotOutput("age_sex"))
   ),
-  fluidRow( # Randomized "stories"
-    column(2, actionButton("story", "Tell me a story")),
-    column(10, textOutput("narrative"))
-  )
+  fluidRow( # Question 4
+    column(2, actionButton("prev_story", label = tagList(icon("arrow-left"),"Previous"))),
+    column(2, 
+           actionButton("next_story", label = tagList(icon("arrow-right"), "Next")))
+           ,
+    column(8, textOutput("narrative"))
+  ),
+  downloadButton("download_summary", "Download Summary") # New Stuff
+  
   
 )
 
@@ -47,13 +137,14 @@ server <- function(input, output, session) {
   
 ## Output for each column of the table - Altered code to make it pretty
   output$diag <- renderTable({
-    count_top(selected(), diag)
+    count_top(selected(), diag, n = input$n_rows) #input$n_rows goes with the numberInput() in the ui
   }, striped = TRUE, hover = TRUE)
+  
   output$body_part <- renderTable({
-    count_top(selected(), diag)
+    count_top(selected(), diag, n = input$n_rows)
   }, striped = TRUE, hover = TRUE)
   output$location <- renderTable({
-    count_top(selected(), diag)
+    count_top(selected(), diag, n = input$n_rows)
   }, striped = TRUE, hover = TRUE)
   
 ## Creating object that holds information for plotting
@@ -81,15 +172,40 @@ server <- function(input, output, session) {
     }
   }, res = 96)
   
-## Object for the story
-  narrative_sample <- eventReactive(
-    list(input$story, selected()),
-    selected() %>% pull(narrative) %>% sample(1)
+## Store the list of narratives and current index
+  narratives <- reactive({
+    selected() %>% pull(narrative)
+  })
+  current_index <- reactiveVal(1)
+
+## Question 4 
+  observeEvent(input$next_story, {
+    if (current_index() < length(narratives())) {
+      current_index(current_index() + 1)
+    }
+  })
+  
+  observeEvent(input$prev_story, {
+    if (current_index() > 1) {
+      current_index(current_index() - 1)
+    }
+  })
+  
+  output$narrative <- renderText({
+    if (length(narratives()) == 0) return("No narratives available.")
+    narratives()[current_index()]
+  })
+  
+  
+  output$download_summary <- downloadHandler( # New stuff
+    filename = function() {
+      paste("summary-", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(summary(), file, row.names = FALSE)
+    }
   )
   
-## Story Output
-  output$narrative <- renderText(
-    narrative_sample())
 }
 
 # Run the application 
